026e1d41473bdc1da4e7655dcdab594f96d62d0d
Leethring <15216601917@163.com>
2019-10-15 07:07:03 +0000

    varialbe

---
+++ variable.py
@@ -0,0 +1,12 @@
+a = 123  # a is int
+print(a)
+a = 'ABC' #  a is a string
+print(a)
+n = 123
+f = 456.789
+s1 = 'Hello.,world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print(a,n,f,s1,s2,s3,s4)
